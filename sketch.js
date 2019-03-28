72f865fe23eb9e9e014638f61fd25d33810bf493
WDaan <dw.daanwijns@gmail.com>
2019-03-28 20:14:37 +0100

    bugfixes 2.0

--- sketch.js
+++ sketch.js
@@ -4,344 +4,330 @@
 // Steering Evolution
 // Another version:
 // https://github.com/shiffman/NOC-S17-2-Intelligence-Learning/tree/master/week2-evolution/01_evolve_steering
-var vehicles = [];
-var food = [];
-var poison = [];
-var dead_vehicles = [];
-var best_vehicles = [];
-var vehicle_id;
-var alive_table;
-var dead_table;
-var best_table;
-var new_food_pois
-var once = true;
+let vehicles = [];
+let food = [];
+let poison = [];
+let dead_vehicles = [];
+let best_vehicles = [];
+let vehicle_id;
+let alive_table;
+let dead_table;
+let best_table;
+let new_food_pois;
+let once = true;
 
 function setup() {
-    var canvas = createCanvas(965, 460);
-    canvas.parent('canvas_panel');
+  const canvas = createCanvas(windowWidth * 0.668, 460); // 965, 460
+  canvas.parent('canvas_panel');
 
-    var num_car = select('#num_car').value(15);
-    var num_food = select('#num_food').value(30);
-    var num_pois = select('#num_pois').value(40);
-    var mr = select('#mr').value(0.01);
-    new_food_pois = select('#new_food_pois').value(1);
+  let num_car = select('#num_car').value(15);
+  let num_food = select('#num_food').value(30);
+  let num_pois = select('#num_pois').value(40);
+  let mr = select('#mr').value(0.01);
+  new_food_pois = select('#new_food_pois').value(1);
 
-    alive_table = document.getElementById("alive_table");
-    dead_table = document.getElementById("dead_table");
-    best_table = document.getElementById("best_table");
+  alive_table = document.getElementById('alive_table');
+  dead_table = document.getElementById('dead_table');
+  best_table = document.getElementById('best_table');
 
+  perception = select('#check_perception');
+  const use_best = select('#use_best');
 
-    perception = select('#check_perception');
-    use_best = select('#use_best');
+  vehicle_id = 1;
 
-    vehicle_id = 1;
+  vehicles.length = 1;
 
-    vehicles.length = 1;
+  stop();
 
-    stop();
+  // init tables
+  create_empty_table(best_table, 10);
+  create_empty_table(alive_table, 5);
 
-    //init tables
-    create_empty_table(best_table, 10);
+  const apply = select('#apply_variables');
+  apply.mousePressed(() => {
+    console.log('Apply Clicked');
+    // get input values
+    num_car = select('#num_car').value();
+    num_food = select('#num_food').value();
+    num_pois = select('#num_pois').value();
+    mr = select('#mr').value();
+    new_food_pois = select('#new_food_pois').value();
+
+    set_mr(mr);
+
+    reinit();
+    start();
+
+    // reset the stop button
+    if (!(STOP.class().indexOf('stop') > -1)) {
+      document.getElementById('STOP').innerHTML = 'STOP';
+      STOP.removeClass('start');
+      STOP.addClass('stop');
+    }
+    // re-enable save button
+    document.getElementById('save').classList.remove('disabled');
+  });
+
+  let STOP = select('#STOP');
+  STOP.mousePressed(() => {
+    if (STOP.class().indexOf('stop') > -1) {
+      stop();
+      STOP.removeClass('stop');
+      STOP.addClass('start');
+      document.getElementById('STOP').innerHTML = 'START';
+    } else {
+      document.getElementById('STOP').innerHTML = 'STOP';
+      start();
+      STOP.removeClass('start');
+      STOP.addClass('stop');
+    }
+  });
+
+  const save = select('#save');
+  save.mousePressed(() => {
+    if (dead_vehicles.length > 0 && vehicles.length === 0) {
+      if (best_vehicles.length === 0) {
+        best_vehicles = dead_vehicles.slice();
+      } else {
+        best_vehicles = best_vehicles.concat(dead_vehicles);
+      }
+
+      best_vehicles.sort((a, b) => {
+        return b.time_alive - a.time_alive;
+      });
+
+      if (best_vehicles.length > 10) {
+        best_vehicles = best_vehicles.slice(0, 10);
+      }
+
+      best_vehicles = [...new Set(best_vehicles)];
+
+      write_table(best_table, best_vehicles, 10);
+      document.getElementById('save').classList.add('disabled');
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('No dead bots yet or some still alive');
+    }
+  });
+
+  const clear = select('#clear');
+  clear.mousePressed(() => {
+    // reset tables
+    delete_table(dead_table);
+    clear_table(alive_table, 5);
+  });
+
+  function init_food_and_poison() {
+    for (let i = 0; i < num_food; i++) {
+      const x = random(width);
+      const y = random(height);
+      food.push(createVector(x, y));
+    }
+    for (let i = 0; i < num_pois; i++) {
+      const x = random(width);
+      const y = random(height);
+      poison.push(createVector(x, y));
+    }
+  }
+
+  function init_random_bots() {
+    document.getElementById('gen_num').innerHTML = 1;
+    for (let i = 0; i < num_car; i++) {
+      const x = random(width);
+      const y = random(height);
+      vehicles[i] = new Vehicle(x, y, null, vehicle_id);
+      vehicle_id++;
+    }
+  }
+
+  function init_bots_using_best() {
+    // increase generation numbers
+    let gen = document.getElementById('gen_num').innerHTML;
+    gen++;
+    document.getElementById('gen_num').innerHTML = gen;
+    select('#gen_num').value(gen);
+    // create new bots using best
+    // not using clone because id's of clones have .number in them => gets very unorganised after a while
+    for (let i = 0; i < num_car; i++) {
+      const x = random(width);
+      const y = random(height);
+      const random_best_dna = best_vehicles[Math.floor(random(best_vehicles.length - 1))].dna;
+      vehicles[i] = new Vehicle(x, y, random_best_dna, vehicle_id);
+      vehicle_id++;
+    }
+  }
+
+  async function reinit() {
+    vehicles = [];
+    food = [];
+    poison = [];
+    dead_vehicles = [];
+    if (use_best.checked()) {
+      init_bots_using_best();
+    } else {
+      init_random_bots();
+    }
+    set_mr(mr);
+
+    // reset tables
+    delete_table(dead_table);
+    delete_table(alive_table);
     create_empty_table(alive_table, 5);
 
-    var apply = select('#apply_variables');
-    apply.mousePressed(() => {
-        console.log('Apply Clicked');
-        //get input values
-        num_car = select('#num_car').value();
-        num_food = select('#num_food').value();
-        num_pois = select('#num_pois').value();
-        mr = select('#mr').value();
-        new_food_pois = select('#new_food_pois').value();
-
-        reinit();
-        start();
-
-        //reset the stop button
-        if (STOP.class().indexOf('stop') > -1) {} else {
-            document.getElementById('STOP').innerHTML = 'STOP';
-            STOP.removeClass('start');
-            STOP.addClass('stop');
-        }
-        //re-enable save button
-        document.getElementById('save').classList.remove('disabled');
-    });
-
-
-
-    var STOP = select('#STOP');
-    STOP.mousePressed(() => {
-        if (STOP.class().indexOf('stop') > -1) {
-            stop();
-            STOP.removeClass('stop');
-            STOP.addClass('start');
-            document.getElementById('STOP').innerHTML = 'START';
-        } else {
-            document.getElementById('STOP').innerHTML = 'STOP';
-            start();
-            STOP.removeClass('start');
-            STOP.addClass('stop');
-        }
-    });
-
-
-    var save = select('#save');
-    save.mousePressed(() => {
-        if (dead_vehicles.length > 0 && vehicles.length == 0) {
-            if (best_vehicles.length == 0) {
-                best_vehicles = dead_vehicles.slice();
-            } else {
-                best_vehicles = best_vehicles.concat(dead_vehicles);
-            }
-
-            best_vehicles.sort((a, b) => {
-                return (b.time_alive - a.time_alive);
-            });
-
-
-            if (best_vehicles.length > 10) {
-                best_vehicles = best_vehicles.slice(0, 10);
-            }
-
-
-
-            best_vehicles = [...new Set(best_vehicles)];
-
-            write_table(best_table, best_vehicles, 10);
-            document.getElementById('save').classList.add('disabled');
-        } else {
-            alert('No dead bots yet or some still alive');
-        }
-    });
-
-    var clear = select('#clear');
-    clear.mousePressed(() => {
-        //reset tables
-        delete_table(dead_table);
-        clear_table(alive_table, 5);
-    })
-
-    function init_food_and_poison() {
-        for (var i = 0; i < num_food; i++) {
-            var x = random(width);
-            var y = random(height);
-            food.push(createVector(x, y));
-        }
-        for (var i = 0; i < num_pois; i++) {
-            var x = random(width);
-            var y = random(height);
-            poison.push(createVector(x, y));
-        }
-    }
-
-    function init_random_bots() {
-        document.getElementById("gen_num").innerHTML = 1;
-        for (var i = 0; i < num_car; i++) {
-            var x = random(width);
-            var y = random(height);
-            vehicles[i] = new Vehicle(x, y, null, vehicle_id);
-            vehicle_id++;
-        }
-    }
-
-    function init_bots_using_best() {
-        //increase generation numbers
-        var gen = document.getElementById("gen_num").innerHTML;
-        gen++;
-        document.getElementById("gen_num").innerHTML = gen;
-        select('#gen_num').value(gen);
-        //create new bots using best
-        //not using clone because id's of clones have .number in them => gets very unorganised after a while
-        for (var i = 0; i < num_car; i++) {
-            var x = random(width);
-            var y = random(height);
-            var random_best_dna = best_vehicles[Math.floor(random(best_vehicles.length - 1))].dna;
-            vehicles[i] = new Vehicle(x, y, random_best_dna, vehicle_id);
-            vehicle_id++;
-        }
-    }
-
-    async function reinit() {
-        vehicles = [];
-        food = [];
-        poison = [];
-        dead_vehicles = [];
-        if (use_best.checked()) {
-            init_bots_using_best();
-        } else {
-            init_random_bots();
-        }
-        set_mr(mr);
-
-
-        //reset tables
-        delete_table(dead_table);
-        delete_table(alive_table);
-        create_empty_table(alive_table, 5);
-
-        init_food_and_poison();
-        draw();
-    }
-
+    init_food_and_poison();
+    draw();
+  }
 }
 
 async function draw() {
-    background(51);
+  background(51);
 
-    if (vehicles.length > 0) {
-        if (random(1) < (new_food_pois / 10)) {
-            var x = random(width);
-            var y = random(height);
-            food.push(createVector(x, y));
-        }
-        if (random(1) < (new_food_pois / 50)) {
-            var x = random(width);
-            var y = random(height);
-            poison.push(createVector(x, y));
-        }
+  if (vehicles.length > 0) {
+    if (random(1) < new_food_pois / 10) {
+      const x = random(width);
+      const y = random(height);
+      food.push(createVector(x, y));
     }
-    for (var i = 0; i < food.length; i++) {
-        fill(0, 255, 0);
-        noStroke();
-        ellipse(food[i].x, food[i].y, 4, 4);
+    if (random(1) < new_food_pois / 50) {
+      const x = random(width);
+      const y = random(height);
+      poison.push(createVector(x, y));
     }
-    for (var i = 0; i < poison.length; i++) {
-        fill(255, 0, 0);
-        noStroke();
-        ellipse(poison[i].x, poison[i].y, 4, 4);
+  }
+  for (let i = 0; i < food.length; i++) {
+    fill(0, 255, 0);
+    noStroke();
+    ellipse(food[i].x, food[i].y, 4, 4);
+  }
+  for (let i = 0; i < poison.length; i++) {
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(poison[i].x, poison[i].y, 4, 4);
+  }
+  for (let i = vehicles.length - 1; i >= 0; i--) {
+    if (vehicles[i] instanceof Vehicle) {
+      vehicles[i].boundaries();
+      vehicles[i].behaviors(food, poison);
+      vehicles[i].update();
+      vehicles[i].display();
+      const newVehicle = vehicles[i].clone();
+      if (newVehicle != null) {
+        vehicles.push(newVehicle);
+      }
+      if (vehicles[i].dead()) {
+        dead_vehicles.push(vehicles[i]);
+        const x = vehicles[i].position.x;
+        const y = vehicles[i].position.y;
+        food.push(createVector(x, y));
+        vehicles.splice(i, 1);
+      }
     }
-    for (var i = vehicles.length - 1; i >= 0; i--) {
-        if (vehicles[i] instanceof Vehicle) {
-            vehicles[i].boundaries();
-            vehicles[i].behaviors(food, poison);
-            vehicles[i].update();
-            vehicles[i].display();
-            var newVehicle = vehicles[i].clone();
-            if (newVehicle != null) {
-                vehicles.push(newVehicle);
-            }
-            if (vehicles[i].dead()) {
-                dead_vehicles.push(vehicles[i]);
-                var x = vehicles[i].position.x;
-                var y = vehicles[i].position.y;
-                food.push(createVector(x, y));
-                vehicles.splice(i, 1);
-            }
-        }
-    }
+  }
 
-    if (vehicles.length === 0) {
-        dead_vehicles.sort((a, b) => {
-            return a.time_alive - b.time_alive;
-        })
-        if (once) {
-            once = false;
-            dead_vehicles.sort((a, b) => {
-                return b.time_alive - a.time_alive;
-            })
-            create_empty_table(dead_table, dead_vehicles.length);
-            write_table(dead_table, dead_vehicles, dead_vehicles.length);
-            clear_table(alive_table, 5);
-            await wait(500);
-            stop();
-        }
-
+  if (vehicles.length === 0) {
+    if (once) {
+      once = false;
+      dead_vehicles.sort((a, b) => {
+        return b.time_alive - a.time_alive;
+      });
+      create_empty_table(dead_table, 5);
+      write_table(dead_table, dead_vehicles, 5);
+      clear_table(alive_table, 5);
+      await wait(500);
+      stop();
     }
+  }
 
-    if (vehicles.length >= 1) {
-        vehicles.sort((a, b) => {
-            return b.time_alive - a.time_alive;
-        })
-        write_table(alive_table, vehicles, 5);
-    }
+  if (vehicles.length >= 1) {
+    vehicles.sort((a, b) => {
+      return b.time_alive - a.time_alive;
+    });
+    write_table(alive_table, vehicles, 5);
+  }
 
-
-    document.getElementById('curr_num_alive').innerHTML = vehicles.length;
+  document.getElementById('curr_num_alive').innerHTML = vehicles.length;
 }
 
 function wait(num) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('resolved');
-        }, num);
-    });
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, num);
+  });
 }
 
 function stop() {
-    noLoop();
+  noLoop();
 }
 
 function start() {
-    loop();
+  loop();
 }
 
 function delete_table(table) {
-    for (var i = table.rows.length - 1; i > 0; i--) {
-        table.deleteRow(i);
-    }
+  for (let i = table.rows.length - 1; i > 0; i--) {
+    table.deleteRow(i);
+  }
 }
 
 function create_empty_table(table, num_el) {
-    for (i = 1; i < num_el + 1; i++) {
-        var row = table.insertRow();
+  for (let i = 1; i < num_el + 1; i++) {
+    const row = table.insertRow();
 
-        for (j = 0; j < 7; j++) {
-            var cell = row.insertCell();
-            cell.innerHTML = "";
-        }
+    for (let j = 0; j < 7; j++) {
+      const cell = row.insertCell();
+      cell.innerHTML = '';
     }
+  }
 }
 
 function clear_table(table, num_el, start = 0) {
-    if (num_el > table.rows.length - 1) { //cuzz not counting tableheader
-        num_el = table.rows.length - 1;
+  if (num_el > table.rows.length - 1) {
+    // cuzz not counting tableheader
+    num_el = table.rows.length - 1;
+  }
+  for (let i = 0; i < num_el; i++) {
+    const row = table.rows[start + i + 1];
+    for (let j = 0; j < 7; j++) {
+      const cell = row.cells[j];
+      cell.innerHTML = '';
     }
-    for (var i = 0; i < num_el; i++) {
-        var row = table.rows[start + i + 1];
-        for (j = 0; j < 7; j++) {
-            var cell = row.cells[j];
-            cell.innerHTML = "";
-        }
-    }
+  }
 }
 
-
-
 async function write_table(table, arr, num_el) {
-    var excess = 0;
-    if (num_el > arr.length) {
-        excess = num_el - arr.length;
-        num_el = arr.length;
+  let excess = 0;
+  if (num_el > arr.length) {
+    excess = num_el - arr.length;
+    num_el = arr.length;
+  }
+  if (arr.length > 0) {
+    for (let i = 0; i < num_el; i++) {
+      if (arr[i] instanceof Vehicle) {
+        const row = table.rows[i + 1];
+
+        const index = row.cells[0];
+        index.innerHTML = arr[i].id;
+
+        const food_weigth = row.cells[1];
+        food_weigth.innerHTML = Math.round(arr[i].dna.food_weight * 10000) / 10000;
+
+        const poison_weight = row.cells[2];
+        poison_weight.innerHTML = Math.round(arr[i].dna.poison_weight * 10000) / 10000;
+
+        const food_perception = row.cells[3];
+        food_perception.innerHTML = Math.round(arr[i].dna.food_perception * 10000) / 10000;
+
+        const poison_perception = row.cells[4];
+        poison_perception.innerHTML = Math.round(arr[i].dna.poison_perception * 10000) / 10000;
+
+        const health = row.cells[5];
+        health.innerHTML = Math.round(arr[i].health * 100) / 100;
+
+        const time = row.cells[6];
+        time.innerHTML = Math.round(arr[i].time_alive * 100) / 100;
+      }
     }
-    if (arr.length > 0) {
-        for (var i = 0; i < num_el; i++) {
-            if (arr[i] instanceof Vehicle) {
-                var row = table.rows[i + 1];
-
-                var index = row.cells[0];
-                index.innerHTML = arr[i].id;
-
-                var food_weigth = row.cells[1];
-                food_weigth.innerHTML = Math.round(arr[i].dna.food_weight * 10000) / 10000;
-
-                var poison_weight = row.cells[2];
-                poison_weight.innerHTML = Math.round(arr[i].dna.poison_weight * 10000) / 10000;
-
-                var food_perception = row.cells[3];
-                food_perception.innerHTML = Math.round(arr[i].dna.food_perception * 10000) / 10000;
-
-                var poison_perception = row.cells[4];
-                poison_perception.innerHTML = Math.round(arr[i].dna.poison_perception * 10000) / 10000;
-
-
-                var health = row.cells[5];
-                health.innerHTML = Math.round(arr[i].health * 100) / 100;
-
-
-                var time = row.cells[6];
-                time.innerHTML = Math.round(arr[i].time_alive * 100) / 100;
-            }
-        }
-        clear_table(alive_table, excess, num_el);
-    }
+    clear_table(alive_table, excess, num_el);
+  }
 }
--- vehicle.js
+++ vehicle.js
@@ -12,13 +12,11 @@
 // Part 4: https://youtu.be/ykOcaInciBI
 // Part 5: https://youtu.be/VnFF5V5DS8s
 
-var mr;
+let mr;
 
 function set_mr(num) {
   mr = num;
 }
-
-
 
 function Vehicle(x, y, dna, id) {
   this.acceleration = createVector(0, 0);
@@ -32,14 +30,14 @@ function Vehicle(x, y, dna, id) {
   this.id = id;
   this.offspring_id = 0;
 
-  var that = this;
+  const that = this;
 
   this.dna = {
     food_weight: 0,
     poison_weight: 0,
     food_perception: 0,
     poison_perception: 0
-  }
+  };
   if (dna == null) {
     // Food weight
     this.dna.food_weight = random(-2, 2);
@@ -69,25 +67,21 @@ function Vehicle(x, y, dna, id) {
     }
   }
 
-  this.add_seconds = function () {
+  this.add_seconds = () => {
     this.time_alive += 1;
     console.log(this.time_alive);
-  }
+  };
 
-  this.timer = setInterval(function () {
-      that.time_alive += 1;
-    },
-    1000);
+  this.timer = setInterval(() => {
+    that.time_alive += 1;
+  }, 1000);
 
-  this.stop_time = function (timerId) {
+  this.stop_time = timerId => {
     clearInterval(timerId);
-  }
-
-
+  };
 
   // Method to update location
-  this.update = function () {
-
+  this.update = () => {
     this.health -= 0.005;
 
     // Update velocity
@@ -97,50 +91,46 @@ function Vehicle(x, y, dna, id) {
     this.position.add(this.velocity);
     // Reset accelerationelertion to 0 each cycle
     this.acceleration.mult(0);
-  }
+  };
 
-  this.applyForce = function (force) {
+  this.applyForce = force => {
     // We could add mass here if we want A = F / M
     this.acceleration.add(force);
-  }
+  };
 
-  this.behaviors = function (good, bad) {
-    var steerG = this.eat(good, 0.2, this.dna.food_perception);
-    var steerB = this.eat(bad, -1, this.dna.poison_perception);
+  this.behaviors = (good, bad) => {
+    const steerG = this.eat(good, 0.2, this.dna.food_perception);
+    const steerB = this.eat(bad, -1, this.dna.poison_perception);
 
     steerG.mult(this.dna.food_weight);
     steerB.mult(this.dna.poison_weight);
 
     this.applyForce(steerG);
     this.applyForce(steerB);
-  }
+  };
 
-  this.clone = function () {
-
+  this.clone = () => {
     if (random(1) < 0.002 && this.health > 0.65 && this.time_alive > 10) {
       this.offspring_id++;
-      var id = this.id + "." + this.offspring_id;
-      var nv = new Vehicle(this.position.x, this.position.y, this.dna, id);
+      const id = `${this.id}.${this.offspring_id}`;
+      const nv = new Vehicle(this.position.x, this.position.y, this.dna, id);
       return nv;
-    } else {
-      return null;
     }
-  }
+    return null;
+  };
 
-  this.eat = function (list, nutrition, perception) {
-    var record = Infinity;
-    var closest = null;
-    for (var i = list.length - 1; i >= 0; i--) {
-      var d = this.position.dist(list[i]);
+  this.eat = (list, nutrition, perception) => {
+    let record = Infinity;
+    let closest = null;
+    for (let i = list.length - 1; i >= 0; i--) {
+      const d = this.position.dist(list[i]);
 
-      if (d < this.maxspeed) {
+      if (d < this.maxspeed && this.health <= 5) {
         list.splice(i, 1);
         this.health += nutrition;
-      } else {
-        if (d < record && d < perception) {
-          record = d;
-          closest = list[i];
-        }
+      } else if (d < record && d < perception) {
+        record = d;
+        closest = list[i];
       }
     }
 
@@ -151,43 +141,39 @@ function Vehicle(x, y, dna, id) {
     }
 
     return createVector(0, 0);
-  }
+  };
 
   // A method that calculates a steering force towards a target
   // STEER = DESIRED MINUS VELOCITY
-  this.seek = function (target) {
-
-    var desired = p5.Vector.sub(target, this.position); // A vector pointing from the location to the target
+  this.seek = target => {
+    const desired = p5.Vector.sub(target, this.position); // A vector pointing from the location to the target
 
     // Scale to maximum speed
     desired.setMag(this.maxspeed);
 
     // Steering = Desired minus velocity
-    var steer = p5.Vector.sub(desired, this.velocity);
+    const steer = p5.Vector.sub(desired, this.velocity);
     steer.limit(this.maxforce); // Limit to maximum steering force
 
     return steer;
-    //this.applyForce(steer);
-  }
+    // this.applyForce(steer);
+  };
 
-  this.dead = function () {
-
+  this.dead = () => {
     if (this.health < 0) {
       this.stop_time(this.timer);
       return true;
-    } else {
-      return false;
     }
-  }
+    return false;
+  };
 
-  this.display = function () {
+  this.display = () => {
     // Draw a triangle rotated in the direction of velocity
-    var angle = this.velocity.heading() + PI / 2;
+    const angle = this.velocity.heading() + PI / 2;
 
     push();
     translate(this.position.x, this.position.y);
     rotate(angle);
-
 
     if (perception.checked()) {
       strokeWeight(3);
@@ -201,9 +187,9 @@ function Vehicle(x, y, dna, id) {
       ellipse(0, 0, this.dna.poison_perception * 2);
     }
 
-    var gr = color(0, 255, 0);
-    var rd = color(255, 0, 0);
-    var col = lerpColor(rd, gr, this.health);
+    const gr = color(0, 255, 0);
+    const rd = color(255, 0, 0);
+    const col = lerpColor(rd, gr, this.health);
 
     fill(col);
     stroke(col);
@@ -215,13 +201,12 @@ function Vehicle(x, y, dna, id) {
     endShape(CLOSE);
 
     pop();
-  }
+  };
 
+  this.boundaries = () => {
+    const d = 25;
 
-  this.boundaries = function () {
-    var d = 25;
-
-    var desired = null;
+    let desired = null;
 
     if (this.position.x < d) {
       desired = createVector(this.maxspeed, this.velocity.y);
@@ -238,9 +223,9 @@ function Vehicle(x, y, dna, id) {
     if (desired !== null) {
       desired.normalize();
       desired.mult(this.maxspeed);
-      var steer = p5.Vector.sub(desired, this.velocity);
+      const steer = p5.Vector.sub(desired, this.velocity);
       steer.limit(this.maxforce);
       this.applyForce(steer);
     }
-  }
+  };
 }
